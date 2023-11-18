f1d2e6352aae8cd0d540bc6b52d065348377bf1d
PhucNST.Dev <93502834+siphuc123@users.noreply.github.com>
2023-11-18 23:54:34 +0700

    (React) Update new files
    + Tối ưu CSS.
    + Thêm Collection stats

---
+++ src/pages/collection/collectionstat.jsx
@@ -0,0 +1,232 @@
+import React from "react";
+import "./collectionstats.css";
+import { Link } from "react-router-dom";
+
+// Icon
+import { AiFillStar } from "react-icons/ai";
+
+const CollectionStat = () => {
+  return (
+    <>
+      <div className="collection_stats">
+        <div className="collectionstats">
+          <h1 className="collection-title">Collection stats</h1>
+          <div className="button_collectionstats">
+            <button className="btntrending active">Trending</button>
+            <button className="btntop">Top</button>
+            <button className="btnwatchlist">Watch List</button>
+          </div>
+        </div>
+
+        <div className="allchains">
+          <div className="allchainssol"></div>
+          <div className="allchainssol mt-3">
+            <div className="allchainssoltable">
+              <div className="allchainssoltable3">
+                <Link to="">
+                  <div className="stt">
+                    <h4>#</h4>
+                    <span>Collection</span>
+                  </div>
+                  <div className="name"></div>
+                  <div className="volume">
+                    Volume <i className="fa-regular fa-angles-up-down"></i>
+                  </div>
+                  <div className="change">
+                    % Change <i className="fa-regular fa-angles-up-down"></i>
+                  </div>
+                  <div className="floorprice">
+                    Floor price <i className="fa-regular fa-angles-up-down"></i>
+                  </div>
+                  <div className="sales">
+                    Sales <i className="fa-regular fa-angles-up-down"></i>
+                  </div>
+                  <div className="iconstar"></div>
+                </Link>
+              </div>
+              <div className="allchainssoltable2">
+                <Link to="">
+                  <div className="stt">
+                    <h4>1</h4>
+                    <img src="img/collectimg2.jpg" alt="" />
+                  </div>
+                  <div className="name">Name</div>
+                  <div className="volume">0.001 SOL</div>
+                  <div className="change">--</div>
+                  <div className="floorprice">0.00 SOL</div>
+                  <div className="sales">1</div>
+                  <div className="iconstar">
+                    <AiFillStar />
+                  </div>
+                </Link>
+              </div>
+              <div className="allchainssoltable2">
+                <Link to="">
+                  <div className="stt">
+                    <h4>2</h4>
+                    <img src="img/collectimg.jpg" alt="" />
+                  </div>
+                  <div className="name">Name</div>
+                  <div className="volume">0.001 SOL</div>
+                  <div className="change">--</div>
+                  <div className="floorprice">0.00 SOL</div>
+                  <div className="sales">1</div>
+                  <div className="iconstar">
+                    <AiFillStar />
+                  </div>
+                </Link>
+              </div>
+              <div className="allchainssoltable2">
+                <Link to="">
+                  <div className="stt">
+                    <h4>3</h4>
+                    <img src="img/collectimg3.jpg" alt="" />
+                  </div>
+                  <div className="name">Name</div>
+                  <div className="volume">0.001 SOL</div>
+                  <div className="change">--</div>
+                  <div className="floorprice">0.00 SOL</div>
+                  <div className="sales">1</div>
+                  <div className="iconstar">
+                    <AiFillStar />
+                  </div>
+                </Link>
+              </div>
+              <div className="allchainssoltable2">
+                <Link to="">
+                  <div className="stt">
+                    <h4>4</h4>
+                    <img src="img/collectimg.jpg" alt="" />
+                  </div>
+                  <div className="name">Name</div>
+                  <div className="volume">0.001 SOL</div>
+                  <div className="change">--</div>
+                  <div className="floorprice">0.00 SOL</div>
+                  <div className="sales">1</div>
+                  <div className="iconstar">
+                    <AiFillStar />
+                  </div>
+                </Link>
+              </div>
+              <div className="allchainssoltable2">
+                <Link to="">
+                  <div className="stt">
+                    <h4>5</h4>
+                    <img src="img/collectimg3.jpg" alt="" />
+                  </div>
+                  <div className="name">Name</div>
+                  <div className="volume">0.001 SOL</div>
+                  <div className="change">--</div>
+                  <div className="floorprice">0.00 SOL</div>
+                  <div className="sales">1</div>
+                  <div className="iconstar">
+                    <AiFillStar />
+                  </div>
+                </Link>
+              </div>
+
+              <div className="allchainssoltable2">
+                <Link to="">
+                  <div className="stt">
+                    <h4>6</h4>
+                    <img src="img/collectimg3.jpg" alt="" />
+                  </div>
+                  <div className="name">Name</div>
+                  <div className="volume">0.001 SOL</div>
+                  <div className="change">--</div>
+                  <div className="floorprice">0.00 SOL</div>
+                  <div className="sales">1</div>
+                  <div className="iconstar">
+                    <AiFillStar />
+                  </div>
+                </Link>
+              </div>
+              <div className="allchainssoltable2">
+                <Link to="">
+                  <div className="stt">
+                    <h4>7</h4>
+                    <img src="img/collectimg.jpg" alt="" />
+                  </div>
+                  <div className="name">Name</div>
+                  <div className="volume">0.001 SOL</div>
+                  <div className="change">--</div>
+                  <div className="floorprice">0.00 SOL</div>
+                  <div className="sales">1</div>
+                  <div className="iconstar">
+                    <AiFillStar />
+                  </div>
+                </Link>
+              </div>
+              <div className="allchainssoltable2">
+                <Link to="">
+                  <div className="stt">
+                    <h4>8</h4>
+                    <img src="img/collectimg.jpg" alt="" />
+                  </div>
+                  <div className="name">Name</div>
+                  <div className="volume">0.001 SOL</div>
+                  <div className="change">--</div>
+                  <div className="floorprice">0.00 SOL</div>
+                  <div className="sales">1</div>
+                  <div className="iconstar">
+                    <AiFillStar />
+                  </div>
+                </Link>
+              </div>
+              <div className="allchainssoltable2">
+                <Link to="">
+                  <div className="stt">
+                    <h4>9</h4>
+                    <img src="img/collectimg.jpg" alt="" />
+                  </div>
+                  <div className="name">Name</div>
+                  <div className="volume">0.001 SOL</div>
+                  <div className="change">--</div>
+                  <div className="floorprice">0.00 SOL</div>
+                  <div className="sales">1</div>
+                  <div className="iconstar">
+                    <AiFillStar />
+                  </div>
+                </Link>
+              </div>
+              <div className="allchainssoltable2">
+                <Link to="">
+                  <div className="stt">
+                    <h4>10</h4>
+                    <img src="img/collectimg2.jpg" alt="" />
+                  </div>
+                  <div className="name">Name</div>
+                  <div className="volume">0.001 SOL</div>
+                  <div className="change">--</div>
+                  <div className="floorprice">0.00 SOL</div>
+                  <div className="sales">1</div>
+                  <div className="iconstar">
+                    <AiFillStar />
+                  </div>
+                </Link>
+              </div>
+              <div className="allchainssoltable2">
+                <Link to="">
+                  <div className="stt">
+                    <h4>11</h4>
+                    <img src="../" alt="" />
+                  </div>
+                  <div className="name">Name</div>
+                  <div className="volume">0.001 SOL</div>
+                  <div className="change">--</div>
+                  <div className="floorprice">0.00 SOL</div>
+                  <div className="sales">1</div>
+                  <div className="iconstar">
+                    <AiFillStar />
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CollectionStat;
